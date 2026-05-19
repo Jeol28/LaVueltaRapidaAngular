2d228515a710c958ad55fb3bd37f6afd330750d2
Jeol28 <jorgeolaya2803@gmail.com>
2026-05-19 16:40:28 -0500

    Doble feliz

--- src/app/landing-page/landing/landing.component.ts
+++ src/app/landing-page/landing/landing.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-landing',
   templateUrl: './landing.component.html',
   styleUrls: ['./landing.component.css']
 })
-export class LandingComponent {
+export class LandingComponent implements OnInit {
 
+  constructor(private router: Router) {}
+
+  ngOnInit(): void {
+    const token = localStorage.getItem('token');
+    if (!token) return;
+
+    const role = localStorage.getItem('role');
+    if (role === 'admin') {
+      this.router.navigate(['/admin/comidas']);
+    } else if (role === 'operador') {
+      this.router.navigate(['/operador/inicio']);
+    } else {
+      this.router.navigate(['/perfil']);
+    }
+  }
 }
